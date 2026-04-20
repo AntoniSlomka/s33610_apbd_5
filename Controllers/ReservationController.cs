e6e4571019d3177dfe9beee46909962571db727a
Antoni Słomka <s33610@pjwstk.edu.pl>
2026-04-20 22:10:42 +0200

    Added POST method for ReservationController

--- Controllers/ReservationController.cs
+++ Controllers/ReservationController.cs
@@ -11,7 +11,7 @@ namespace Apbd5.Controllers
         // x GET /api/reservations/{id} Returns a single reservation
         // x GET /api/reservations?date=2026-05-10&status=confirmed&roomId=2
         //   Returns reservations filtered by query string parameters
-        // POST /api/reservations Creates a new reservation
+        // x POST /api/reservations Creates a new reservation
         // PUT /api/reservations/{id} Updates an existing reservation
         // DELETE /api/reservations/{id} Deletes a reservation
 
@@ -52,7 +52,20 @@ namespace Apbd5.Controllers
             return Ok(room);
         }
 
+        [HttpPost]
+        public ActionResult<Reservation> CreateReservation(Reservation reservation)
+        {
+            if (Database.DataStore.Reservations
+                .Exists(r => r.RoomId == reservation.RoomId && r.Date == reservation.Date &&
+                (reservation.StartTime.IsBetween(r.StartTime, r.EndTime) || reservation.EndTime.IsBetween(r.StartTime, r.EndTime))))
+            {
+                return Conflict($"A reservation for room {reservation.RoomId} alread exists between {reservation.StartTime} and {reservation.EndTime}.");
+            }
+            reservation.Id = Database.DataStore.NextReservationId;
+            Database.DataStore.Reservations.Add(reservation);
 
+            return CreatedAtAction(nameof(GetById), new { id = reservation.Id }, reservation);
+        }
 
     }
 }
